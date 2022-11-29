f047a59a661505542a8a649d722a918226b402a3
chaerin2739 <118095564+chaerin2739@users.noreply.github.com>
2022-11-29 18:14:03 +0900

    cmake2
    뒤집어출력하기_STATIC으로

---
+++ exam2/CMakeFiles/reverseCopy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reverseCopy.dir/mmain.c.o"
+  "CMakeFiles/reverseCopy.dir/mmain.c.o.d"
+  "reverseCopy"
+  "reverseCopy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/reverseCopy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ exam2/lib_build/CMakeFiles/reverseCopy.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reverseCopy.dir/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c.o"
+  "CMakeFiles/reverseCopy.dir/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c.o.d"
+  "CMakeFiles/reverseCopy.dir/rreverse.c.o"
+  "CMakeFiles/reverseCopy.dir/rreverse.c.o.d"
+  "libreverseCopy.a"
+  "libreverseCopy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/reverseCopy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
